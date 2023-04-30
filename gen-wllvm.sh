74b09bdbaeb6a74cb858be3ad709bac5ca5515fc
Mingjie Shen <shen497@purdue.edu>
2023-04-30 00:38:04 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-DB=$RTOSExploration/bitcode-db/STM32_BASE
-rm -rf $DB
+#DB=$RTOSExploration/bitcode-db/STM32_BASE
+#rm -rf $DB
 
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export GCC_PATH="$RTOSExploration/toolchain/gcc-arm-none-eabi-10.3-2021.10/bin/"
+export GCC_CROSS_COMPILE_PREFIX=arm-none-eabi-
+export PATH=$RTOSExploration/bin-wrapper:$PATH
 rm -rf build
-mkdir -p build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cross.cmake -DTARGET_GROUP=production
-genbc $DB -c "make -j$(nproc)"
+cmake -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=cross.cmake -DTARGET_GROUP=production
+cmake --build build -j$(nproc)
+extract-bc build/main.elf
